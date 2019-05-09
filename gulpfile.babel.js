1b8bc8d714902bd076eaf1242ecbc2b483923fc4
Overbool <overbool.xu@gmail.com>
2019-05-10 00:27:59 +0800

    chore(server): add gulp-connect

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,40 +1,54 @@
-const gulp = require('gulp')
-const concat = require('gulp-concat')
-const uglify = require('gulp-uglify')
-const babel = require('gulp-babel')
-const header = require('gulp-header')
-const pkg = require('./package.json')
+const gulp = require("gulp");
+const concat = require("gulp-concat");
+const uglify = require("gulp-uglify");
+const babel = require("gulp-babel");
+const header = require("gulp-header");
+const pkg = require("./package.json");
+const connect = require("gulp-connect");
 
 const banner = [
-  '/*! ',
-  '<%= package.name %> ',
-  'v<%= package.version %> | ',
-  '(c) ' + new Date().getFullYear() + ' <%= package.author %> |',
-  ' <%= package.homepage %>',
-  ' */',
-  '\n'
-].join('');
+  "/*! ",
+  "<%= package.name %> ",
+  "v<%= package.version %> | ",
+  "(c) " + new Date().getFullYear() + " <%= package.author %> |",
+  " <%= package.homepage %>",
+  " */",
+  "\n"
+].join("");
 
-gulp.task('js', () => {
-  return gulp.src('src/*.js')
-    .pipe(babel({
-      presets: ['@babel/env']
-    }))
-    .pipe(concat('poster.js'))
-    .pipe(header(banner, {
-      package: pkg
-    }))
-    .pipe(gulp.dest('dist/'))
+gulp.task("js", () => {
+  return gulp
+    .src("src/*.js")
+    .pipe(
+      babel({
+        presets: ["@babel/env"]
+      })
+    )
+    .pipe(concat("poster.js"))
+    .pipe(
+      header(banner, {
+        package: pkg
+      })
+    )
+    .pipe(gulp.dest("dist/"))
     .pipe(uglify())
-    .pipe(concat('poster.min.js'))
-    .pipe(header(banner, {
-      package: pkg
-    }))
-    .pipe(gulp.dest('dist/'))
+    .pipe(concat("poster.min.js"))
+    .pipe(
+      header(banner, {
+        package: pkg
+      })
+    )
+    .pipe(gulp.dest("dist/"));
 });
 
-gulp.watch('src/**/*.js', function(e) {
-  gulp.start('js')
-})
+gulp.task("server", function() {
+  connect.server({
+    port: 8080,
+  });
+});
 
-gulp.task('default', ['js'])
+gulp.watch("src/**/*.js", function(e) {
+  gulp.start("js");
+});
+
+gulp.task("default", ["js", 'server']);
